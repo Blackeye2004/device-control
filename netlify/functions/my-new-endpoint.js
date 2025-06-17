a30fb40ed5465d38b7ec95cbf7b7cbfd67986215
Blackeye2004 <sendithere43@gmail.com>
2025-06-17 07:50:37 +0100

    Delete netlify/functions/my-new-endpoint.js

--- netlify/functions/my-new-endpoint.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/my-new-endpoint.js
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from my new endpoint!" }),
-  };
-};
